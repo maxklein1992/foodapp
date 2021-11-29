7ddac770b00c53b0c1be69ce3319e2a19768c6cb
Max Klein <Max.Klein@evision-software.com>
2021-11-29 23:10:58 +0000

    Height issue fixed

--- src/components/MenuItemCard.js
+++ src/components/MenuItemCard.js
@@ -10,10 +10,10 @@ function MenuItemCard({ id, image, alt, title, price }) {
           className="object-cover w-64 h-48 transform hover:scale-105 ease-out duration-700 rounded-lg shadow-lg"
         />
         <div className="pt-4 pl-3 w-64 flex flex-col">
-          <p className="text-yellow text-xs w-full flex-1">
+          <p className="text-yellow text-xs w-full h-8">
             {id}.{title}
           </p>
-          <p className="text-gray text-sm mt-1 font-medium font-sans flex-1">
+          <p className="text-gray text-sm mt-1 font-medium font-sans h-8">
             € {price}
           </p>
         </div>
